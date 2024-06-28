737f7c159e97fb32e666ec8da56220e650ee65a1
Pacatro <paco.algar.15@gmail.com>
2024-06-28 14:43:24 +0200

    addign link to  doc

--- src/nn.rs
+++ src/nn.rs
@@ -1,3 +1,5 @@
+// See: https://github.com/nmarincic/machineintelligence/blob/master/11.%20Building%20a%20Neural%20Network%20using%20Matrices%20-%20Step%20by%20Step.ipynb
+
 use ndarray::Array1;
 
 use crate::{activation::Activation, layer::Layer};
