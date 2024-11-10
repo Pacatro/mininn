2cc2205c1850c1955f42eb81482f0a67441d4e82
Pacatro <paco.algar.15@gmail.com>
2024-11-10 23:16:14 +0000

    refactoring cross correlation

--- src/layers/conv.rs
+++ src/layers/conv.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array1, Array2, Array3, Array4, ArrayView2};
+use ndarray::{Array1, Array2, Array3, Array4};
 use ndarray_rand::{rand::distributions::Uniform, RandomExt};
 use serde::{Deserialize, Serialize};
 
@@ -11,128 +11,41 @@ pub enum Padding {
     Full,
 }
 
-/// Computes the valid cross-correlation between an input array and a kernel.
-///
-/// # Arguments
-/// * `input` - The input 2D array
-/// * `kernel` - The kernel to convolve with the input
-///
-/// # Returns
-/// * `Result<Array2<f64>, CrossCorrelationError>` - The resulting cross-correlation array or an error
-///
-/// # Examples
-/// ```
-/// use ndarray::array;
-/// let input = array![[1.0, 2.0, 3.0],
-///                    [4.0, 5.0, 6.0],
-///                    [7.0, 8.0, 9.0]];
-/// let kernel = array![[1.0, 0.0],
-///                     [0.0, 1.0]];
-/// let result = cross_correlation_valid(&input, &kernel).unwrap();
-/// ```
-///
-pub(crate) fn cross_correlation_valid(input: &Array2<f64>, kernel: &Array2<f64>) -> Array2<f64> {
+// TODO: IMPROVE THIS
+fn cross_correlation(
+    input: &Array2<f64>,
+    kernel: &Array2<f64>,
+    padding: Padding,
+) -> NNResult<Array2<f64>> {
     let mut sums = Vec::new();
 
-    let (input_rows, input_cols) = input.dim();
-    let (kernel_rows, kernel_cols) = kernel.dim();
+    match padding {
+        Padding::Valid => {
+            let (input_rows, input_cols) = input.dim();
+            let (kernel_rows, kernel_cols) = kernel.dim();
 
-    let valid_rows = input_rows - kernel_rows + 1;
-    let valid_cols = input_cols - kernel_cols + 1;
+            let valid_rows = input_rows - kernel_rows + 1;
+            let valid_cols = input_cols - kernel_cols + 1;
 
-    for i in 0..valid_rows {
-        for j in 0..valid_cols {
-            let mut sum = 0.0;
+            for i in 0..valid_rows {
+                for j in 0..valid_cols {
+                    let mut sum = 0.0;
 
-            for ki in 0..kernel_rows {
-                for kj in 0..kernel_cols {
-                    sum += input[[i + ki, j + kj]] * kernel[[ki, kj]];
+                    for ki in 0..kernel_rows {
+                        for kj in 0..kernel_cols {
+                            sum += input[[i + ki, j + kj]] * kernel[[ki, kj]];
+                        }
+                    }
+
+                    sums.push(sum);
                 }
             }
 
-            sums.push(sum);
+            Ok(Array2::from_shape_vec((valid_rows, valid_cols), sums)?)
         }
+        Padding::Full => Ok(Array2::zeros((0, 0))),
     }
-
-    Array2::from_shape_vec((valid_rows, valid_cols), sums).unwrap()
 }
-
-// /// Computes the full cross-correlation between an input array and a kernel.
-// /// The output size will be (input_size + kernel_size - 1) in each dimension.
-// ///
-// /// # Arguments
-// /// * `input` - The input 2D array
-// /// * `kernel` - The kernel to convolve with the input
-// ///
-// /// # Returns
-// /// * `Result<Array2<f64>, CrossCorrelationError>` - The resulting cross-correlation array or an error
-// ///
-// /// # Examples
-// /// ```
-// /// use ndarray::array;
-// /// let input = array![[1.0, 2.0],
-// ///                    [3.0, 4.0]];
-// /// let kernel = array![[1.0, 0.5],
-// ///                     [0.5, 0.25]];
-// /// let result = cross_correlation_full(&input, &kernel).unwrap();
-// /// // Result will be 3x3 array with full padding
-// /// ```
-// pub(crate) fn cross_correlation_full(input: &Array2<f64>, kernel: &Array2<f64>) -> Array2<f64> {
-//     let (input_rows, input_cols) = input.dim();
-//     let (kernel_rows, kernel_cols) = kernel.dim();
-
-//     let output_rows = input_rows + kernel_rows - 1;
-//     let output_cols = input_cols + kernel_cols - 1;
-
-//     let mut output = Array2::zeros((output_rows, output_cols));
-
-//     let pad_top = kernel_rows - 1;
-//     let pad_left = kernel_cols - 1;
-
-//     let input_view = input.view();
-//     let kernel_view = kernel.view();
-
-//     for i in 0..output_rows {
-//         for j in 0..output_cols {
-//             let sum = compute_padded_window_sum(input_view, kernel_view, i, j, pad_top, pad_left);
-//             output[[i, j]] = sum;
-//         }
-//     }
-
-//     output
-// }
-
-// /// Computes the sum of element-wise multiplication between the kernel and the corresponding
-// /// section of the input array, taking padding into account
-// #[inline]
-// fn compute_padded_window_sum(
-//     input: ArrayView2<f64>,
-//     kernel: ArrayView2<f64>,
-//     out_row: usize,
-//     out_col: usize,
-//     pad_top: usize,
-//     pad_left: usize,
-// ) -> f64 {
-//     let (kernel_rows, kernel_cols) = kernel.dim();
-//     let (input_rows, input_cols) = input.dim();
-//     let mut sum = 0.0;
-
-//     for ki in 0..kernel_rows {
-//         for kj in 0..kernel_cols {
-//             // Calcular la posición correspondiente en el input
-//             let input_row = out_row.saturating_sub(pad_top) + ki;
-//             let input_col = out_col.saturating_sub(pad_left) + kj;
-
-//             // Verificar si la posición está dentro del input
-//             if input_row < input_rows && input_col < input_cols {
-//                 sum += input[[input_row, input_col]]
-//                     * kernel[[kernel_rows - 1 - ki, kernel_cols - 1 - kj]];
-//             }
-//         }
-//     }
-
-//     sum
-// }
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub struct Conv {
@@ -245,8 +158,9 @@ mod tests {
     fn test_cross_correlation_valid() {
         let input = array![[1., 6., 2.], [5., 3., 1.], [7., 0., 4.],];
         let kernel = array![[1., 2.], [-1., 0.]];
-        let output = cross_correlation_valid(&input, &kernel);
-        assert_eq!(output, array![[8., 7.], [4., 5.]]);
+        let output = cross_correlation(&input, &kernel, Padding::Valid);
+        assert!(output.is_ok());
+        assert_eq!(output.unwrap(), array![[8., 7.], [4., 5.]]);
     }
 
     // #[test]
