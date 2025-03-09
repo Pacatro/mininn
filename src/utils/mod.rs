3dcd1900dd2f0dadbfb46ab647a5f2fae0a5013c
Pacatro <paco.algar.15@gmail.com>
2025-03-09 17:45:34 +0100

    Remove older conv2d and correlation methods

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -38,19 +38,17 @@
 //!
 //! | Optimizer | Description                                                                                                      |
 //! |-----------|------------------------------------------------------------------------------------------------------------------|
-//! | `GD`      | Gradient Descent. This optimizer updates the weights and biases of the neural network using the gradient of the loss function with respect to the weights and biases. |
+//! | `SGD`      | Stochastic Gradient Descent. This optimizer updates the weights and biases of the neural network using the gradient of the loss function with respect to the weights and biases. |
 //! | `Momentum`| Momentum. This optimizer updates the weights and biases of the neural network using the gradient of the loss function with respect to the weights and biases, but with a momentum term that helps accelerate the learning process. |
 //! | `Adam`    | Adam. This optimizer updates the weights and biases of the neural network using the gradient of the loss function with respect to the weights and biases, but with a momentum term that helps accelerate the learning process and a learning rate that adjusts the step size of the gradient descent. |
 //!
 mod act;
-mod correlation;
 mod cost;
 mod formatting;
 mod metrics;
 mod nn_util;
 mod optimizer;
 
-pub(crate) use correlation::{cross_correlation2d, Padding};
 pub(crate) use optimizer::OptimizerType;
 
 pub use act::{Act, ActCore, ActivationFunction};
